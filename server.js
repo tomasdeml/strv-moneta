db0f67194cf36a6042b1ad82943321aab78f08e1
Tomas Deml <deml.tomas@gmail.com>
2015-01-20 18:37:47 +0100

    WIP Access Token Op

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 var restify = require('restify');
+var firebase = require("firebase");
+var jwt = require('jwt-simple');
+
+var accessTokenSecret = '6i2e{XM(KI?g3I0)jP# (^>8(1o0OY';
+
+function respondWithAccessToken(req, res, next) {
+    if (req.params.email != 'john@doe.org' || req.params.password != 'pass') {
+        res.json(401, { type: 'InvalidEmailPassword', message: 'Specified e-mail / password combination is not valid.' });
+    } else {
+        var payload = { userId: 'bar' };
+        var token = jwt.encode(payload, accessTokenSecret);
+
+        res.json({ access_token: token });
+    }
+    next();
+}
 
 function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
+    //var myFirebaseRef = new Firebase("https://<your-firebase>.firebaseio.com/");
+    
+    
+    res.send('hello ' + req.params.name);
+    next();
 }
 
 var server = restify.createServer();
+server.use(restify.bodyParser());
+server.post('/access_token', respondWithAccessToken);
+
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
-server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
+server.listen(8080, function () {
+    console.log('%s listening at %s', server.name, server.url);
 });
--- test/test.js
+++ test/test.js
@@ -1,9 +1,33 @@
-var assert = require("assert");
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+var assert = require('should');
+var restify = require('restify');
+
+describe('API', function () {
+    var client = restify.createJsonClient({
+        url: 'http://localhost:8080',
+        version: '*'
     });
-  });
+    
+    describe('/access_token', function () {
+        it('should return HTTP 401 error when passed credentials are invalid', function(done) {
+            client.post('/access_token', { email: 'invalid@example.org', password: 'fail' }, function(err, req, res, obj) {
+                res.statusCode.should.equal(401);
+                console.log('%d -> %j', res.statusCode, res.headers);
+                console.log('%j', obj);
+
+                done();
+            });
+        });
+        it('should return JSON with expected *type* property when passed credentials are invalid', function (done) {
+            client.post('/access_token', { email: 'invalid@example.org', password: 'fail' }, function (err, req, res, obj) {
+                obj.should.have.property('type', 'InvalidEmailPassword');
+                done();
+            });
+        });
+        it('should return JSON with expected *message* property when passed credentials are invalid', function (done) {
+            client.post('/access_token', { email: 'invalid@example.org', password: 'fail' }, function (err, req, res, obj) {
+                obj.should.have.property('message', 'Specified e-mail / password combination is not valid.');
+                done();
+            });
+        });
+    });
 });
